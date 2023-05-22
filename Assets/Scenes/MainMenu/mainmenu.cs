2a8396103e467bd70534288fc2d2e6ea0ccf6783
kqlqk <kononchuk99999@gmail.com>
2023-05-22 22:03:53 +0200

    bugfix
    * was fixed all bugs in MainMenu.cs

--- Assets/Scenes/MainMenu/mainmenu.cs
+++ Assets/Scenes/MainMenu/mainmenu.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagment;
-public class mainmenu : MonoBehaviour
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
    {
-    SceneManager.loadScene(SceneManager.GetActiveScene().buildIndex +1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
    }
 
    public void ExitGame()
    {
-       Debug.log("Game Closed");
+       Debug.Log("Game Closed");
        Application.Quit();
    }
 }
